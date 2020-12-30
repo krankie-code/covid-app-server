2707717fce99240246ae21e7689e49858aeb5066
krankie <foresvives10@gmail.com>
2020-12-30 08:12:03 +0000

    restored public

---
+++ public/precache-manifest.79f1326957b836d1dfd80e5fc9f9efdd.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9b818ec29a25883a0e5d55b4322a67b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e5115ad4c24f6663321c",
+    "url": "/static/css/main.2f4fcfce.chunk.css"
+  },
+  {
+    "revision": "24957d291d5fc7753d10",
+    "url": "/static/js/2.d256b98f.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.d256b98f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5115ad4c24f6663321c",
+    "url": "/static/js/main.c60efff5.chunk.js"
+  },
+  {
+    "revision": "ef0c4d834469aab472da",
+    "url": "/static/js/runtime-main.24377fe5.js"
+  },
+  {
+    "revision": "c863f8028c2505f92540e0ba7c379002",
+    "url": "/static/media/PermanentMarker-Regular.c863f802.ttf"
+  },
+  {
+    "revision": "bed7edb681e1c3644fe4c5796abf329d",
+    "url": "/static/media/alcohol.bed7edb6.jpg"
+  },
+  {
+    "revision": "33e1428cfeaf010015fe99ef63dbacf3",
+    "url": "/static/media/face-nose-eyes2.33e1428c.jpg"
+  },
+  {
+    "revision": "a052cc0f7eb5f53b63c9fd6b27eed535",
+    "url": "/static/media/fever.a052cc0f.png"
+  },
+  {
+    "revision": "fff25bad5d04c2e34b4fc221a618c115",
+    "url": "/static/media/hands.fff25bad.jpg"
+  },
+  {
+    "revision": "9115c5125add43ef7c356bfcb2e62411",
+    "url": "/static/media/home.9115c512.jpg"
+  },
+  {
+    "revision": "53a336fe6dbc448a37a9350bdd2fc7b2",
+    "url": "/static/media/—Pngtree—corona virus cartoon vector illustration_5336379.53a336fe.png"
+  },
+  {
+    "revision": "336a58dfd6c2dc180b48db65f92f0ad5",
+    "url": "/static/media/—Pngtree—corona virus cartoon vector_5344879.336a58df.png"
+  },
+  {
+    "revision": "ea3aec879ba2a19c194539fc58897047",
+    "url": "/static/media/—Pngtree—orange mischief virus cute virus_5341821.ea3aec87.png"
+  },
+  {
+    "revision": "72456928d12702616e8e8b839893debd",
+    "url": "/static/media/—Pngtree—yellow virus cartoon covid 19_5386231.72456928.png"
+  }
+]);
